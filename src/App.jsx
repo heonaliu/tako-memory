035c56c4566111c50039d98025c699bcaedc8a5e
Heona <heonaliu@gmail.com>
2026-02-23 13:23:13 -0500

    Game Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { GameHeader } from "./components/GameHeader";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <div className="app">
+      <GameHeader score={3} moves={10} />
+    </div>
+  );
 }
 
 export default App;
